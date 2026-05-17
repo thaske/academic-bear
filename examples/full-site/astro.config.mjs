7528e0b10dcb53b6476e897e8e2e0aa9866e0789
thaske <10328778+thaske@users.noreply.github.com>
2026-05-17 15:22:51 -0400

    Use relative links

--- examples/full-site/astro.config.mjs
+++ examples/full-site/astro.config.mjs
@@ -2,10 +2,11 @@
 
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
+import relativeLinks from "astro-relative-links";
 import { site } from "./src/site.config.ts";
 
 export default defineConfig({
   site: site.url,
   base: "/academic-bear",
-  integrations: [sitemap()],
+  integrations: [relativeLinks(), sitemap()],
 });
